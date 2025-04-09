6087852ed9e8ab452be9a87265ef9175f9fde0c4
sultanr31 <83525246+MSultan-2057051019@users.noreply.github.com>
2025-04-09 15:20:47 +0700

    Icon update

--- src/components/contents/Footer.vue
+++ src/components/contents/Footer.vue
@@ -2,7 +2,7 @@
 
 <template>
   <div class="flex flex-col px-4 md:pt-10 md:px-10 mb-16 md:pr-40 md:mt-16 scale-90 md:scale-100">
-    <p class="text-[12px]">
+    <p class="text-[12px] text-neutral-300">
       Designed with the chaotic precision of a squirrel on espresso in Figma and lovingly duct-taped
       together in Visual Studio Code by yours truly. Fueled by
       <span class="text-green-200">Vue.js</span>, fashionably styled with
